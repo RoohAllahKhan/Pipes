1392600a01acc83c8b6f40ddad32df15c16bb9d0
RoohAllahKhan <roohallahkhan20@gmail.com>
2021-11-17 18:50:20 +0500

    Pure pipes for rendering the new changes along with the filteration.

--- src/app/filter.pipe.ts
+++ src/app/filter.pipe.ts
@@ -1,7 +1,10 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'filter'
+  name: 'filter',
+  // Pure is used if you want to render the changes performed while filteration. for e.g searching for something and adding the
+  // something in the array at the same time. Can lead to performance issues.
+  pure: false
 })
 export class FilterPipe implements PipeTransform {
 
